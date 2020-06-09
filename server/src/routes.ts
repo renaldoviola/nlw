1e4705e46026c01e2d8f19de8f1eb069b078bd09
Renaldo Viola <renaldo@Rocketlab.com.br>
2020-06-09 01:43:16 -0300

    implements react-dropzone

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,13 @@
 import express, { request, response } from 'express'
+import multer from 'multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import multerConfig from './config/multer';
+
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itensController = new ItemsController();
@@ -11,8 +15,11 @@ const itensController = new ItemsController();
 
 routes.get("/items", itensController.index);
 
-routes.post("/points", pointsController.create);
+
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
+
+
+routes.post("/points", upload.single('image'), pointsController.create);
 
 export default routes;
